f38bb1beca175b44b1088f22ef1cf24c42327c4d
duyblx <xuanduy.wonder123@gmail.com>
2023-07-06 08:55:42 +0700

    init code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    html = "<h3>My name: DIENPP. This is my AWS capstone project.</h3>"
+    html = "<h3>Hello, I'm  duyblx. It's my udacity capston project.</h3>"
     return html.format(format)
 
 if __name__ == "__main__":
